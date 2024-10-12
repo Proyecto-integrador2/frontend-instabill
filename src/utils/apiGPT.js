9b3a2e1db2dae935191c796390cd5bff9fce8e19
William Velasco <william.velasco@correounivalle.edu.co>
2024-10-12 10:01:02 -0500

    PI-8 fix calcular totales

--- src/utils/apiGPT.js
+++ src/utils/apiGPT.js
@@ -23,7 +23,7 @@ async function getDataInvoice(dataInvoice){
         messages: [
             {
             role: "system",
-            content: "Eres un asistente que convierte descripciones de compras en JSON con comillas dobles. Campos: nombre, dirección y contacto del cliente, la fecha_facturación (fecha y hora actual, horario UTC-5). Campos: nombre, cantidad, precio_unitario y precio_total de cada producto. Finalmente, calcula el total_compra"
+            content: "Eres un asistente que convierte descripciones de compras en JSON con comillas dobles. Campos: nombre, direccion y contacto del cliente, la fecha_facturacion (fecha y hora actual, horario UTC-5). Campos: nombre, cantidad, precio_unitario y precio_total de cada producto. Finalmente, calcula el total_compra"
             },
             {
             role: "user",
