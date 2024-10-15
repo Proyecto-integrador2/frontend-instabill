f82a25c2b3d44aefa901a17277369aa91efee446
Velyxius <dadawdasd@hotmail.com>
2024-10-15 00:38:28 -0500

    Modified Home Styling to be more Suitable

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './componentes/Home';
-import Invoice from './componentes/Invoice';
-import './App.css'
-import InvoiceList from './componentes/InvoiceList';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./componentes/Home";
+import Invoice from "./componentes/Invoice";
+import "./App.css";
+import InvoiceList from "./componentes/InvoiceList";
 
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/invoice" element={<Invoice />} />
         <Route path="/invoiceList" element={<InvoiceList />} />
-
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
